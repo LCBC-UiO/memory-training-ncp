1ac0595c31726c2f0f9b8752ac6c5278e8b1e8ac
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-02-11 12:42:02 +0100

    updated boot_sampfun so it is able to run case bootstrap with global residuals

--- code/boot_sampfun.R
+++ code/boot_sampfun.R
@@ -1,4 +1,4 @@
-boot_sampfun <- function(fitted, data, response) {
+boot_sampfun <- function(fitted, data, response, individual_residuals = TRUE) {
   dd <- data %>%
     mutate(
       pred = as.numeric(predict(fitted, levels = 1)),
@@ -8,13 +8,20 @@ boot_sampfun <- function(fitted, data, response) {
   iv <- as.character(unique(data$CrossProject_ID))
   bsamp1 <- sample(iv, size = length(iv), replace = TRUE)
   ## within groups, sample *residuals* with replacement
-  map_dfr(bsamp1, function(x) {
-    dd %>%
+  bootdat <- map_dfr(bsamp1, function(x) {
+    dd %>% 
       filter(CrossProject_ID == x)
-  }, .id = "boot_id") %>%
-    group_by(boot_id) %>% # comment this line to get global residual bootstrap
+  }, .id = "boot_id")
+  
+  ## if TRUE, within groups, sample *residuals* with replacement
+  if(individual_residuals){
+    bootdat <- bootdat %>% 
+      group_by(boot_id)
+  }
+  
+  bootdat %>% 
     mutate(
       !!response := pred + sample(res, size = n(), replace = TRUE)
-    ) %>%
+    ) %>% 
     ungroup()
 }
